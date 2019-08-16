0a8c2b5e66ab42b4847234167244a9a4700cd2ad
candyz <648245419@qq.com>
2019-08-16 14:56:44 +0800

    个人信息

--- app/src/main/java/cn/com/lttc/loginui/FirstFragment.java
+++ app/src/main/java/cn/com/lttc/loginui/FirstFragment.java
@@ -1,5 +1,6 @@
 package cn.com.lttc.loginui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,12 +8,76 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import zxing.android.CaptureActivity;
+import zxing.common.Constant;
+
 
 public class FirstFragment extends Fragment {
+    private List<String> data(){
+        //list 中添加数据
+        List<String> data = new ArrayList<>();
+        data.add("扫描二维码");
+        return data;
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.tab1, container, false);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1,data());
+        ListView listView = (ListView)view.findViewById(R.id.list_view);;//在视图中找到ListView
+        listView.setAdapter(arrayAdapter);
+        //为列表视图中选中的项添加响应事件
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String result = parent.getItemAtPosition(position).toString();//获取选择项的值
+                checkItem(result);
+                Toast.makeText(getActivity(), "您点击了" + result, Toast.LENGTH_SHORT).show();
+
+            }
+        });
         return view;
+    }
+    private void checkItem(String result) {
+        //判断选中的条目
+        if("扫描二维码".equals(result)){
+            qrCodeEnter();
+        }
+    }
+    private void qrCodeEnter() {
+        Intent intent = new Intent(getActivity(), CaptureActivity.class);
+        /*ZxingConfig是配置类  可以设置是否显示底部布局，闪光灯，相册，是否播放提示音  震动等动能
+         * 也可以不传这个参数
+         * 不传的话  默认都为默认不震动  其他都为true
+         * */
+
+        //ZxingConfig config = new ZxingConfig();
+        //config.setShowbottomLayout(true);//底部布局（包括闪光灯和相册）
+        //config.setPlayBeep(true);//是否播放提示音
+        //config.setShake(true);//是否震动
+        //config.setShowAlbum(true);//是否显示相册
+        //config.setShowFlashLight(true);//是否显示闪光灯
+        //intent.putExtra(Constant.INTENT_ZXING_CONFIG, config);
+        getActivity().startActivityForResult(intent, 1111);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1111) {
+            if (data != null) {
+                String content = data.getStringExtra(Constant.CODED_CONTENT);
+                Toast.makeText(getActivity(), content, Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
